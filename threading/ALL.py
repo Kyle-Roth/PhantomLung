5e593db1b85b03181cef5dd7bde2a52bf5f9706f
Kyle Roth <kyle.roth.21@gmail.com>
2021-04-21 20:39:52 -0500

    Tabs BS

--- threading/ALL.py
+++ threading/ALL.py
@@ -28,137 +28,137 @@ eTime = []
 
 class MainWindow:
 
-    t = np.linspace(0, 2, 2 * 10000, endpoint=False)
-    f = 1
-    line = None
-    running = 0
-    OFF = 0
-    AMP = 0
-    BPM = 0
-    FUNCinternal = "|sin|"
-    FUNC = "|sin|"
-    UpdateCheck = 0
+	t = np.linspace(0, 2, 2 * 10000, endpoint=False)
+	f = 1
+	line = None
+	running = 0
+	OFF = 0
+	AMP = 0
+	BPM = 0
+	FUNCinternal = "|sin|"
+	FUNC = "|sin|"
+	UpdateCheck = 0
 
-    def __init__(self,root):
+	def __init__(self,root):
 
-        self.root = root
+		self.root = root
 
-        # Set gentle close
-        root.protocol("WM_DELETE_WINDOW", self._close)  # easy kill program
+		# Set gentle close
+		root.protocol("WM_DELETE_WINDOW", self._close)  # easy kill program
 
-        # Create Figure and Pack Axis
-        fig=self.fig = Figure(figsize=(8,6))
-        ax=self.ax = fig.add_subplot(111)
-        canvas=self.canvas = FigureCanvasTkAgg(fig, root) # A tk.DrawingArea
-        self.canvas.get_tk_widget().pack(side='top', fill='both', expand=1) # Add fig to Tk Window
+		# Create Figure and Pack Axis
+		fig=self.fig = Figure(figsize=(8,6))
+		ax=self.ax = fig.add_subplot(111)
+		canvas=self.canvas = FigureCanvasTkAgg(fig, root) # A tk.DrawingArea
+		self.canvas.get_tk_widget().pack(side='top', fill='both', expand=1) # Add fig to Tk Window
 
-        ax.grid(which='both',linestyle='--')
-        ax.set_xlabel("Time (s)")
+		ax.grid(which='both',linestyle='--')
+		ax.set_xlabel("Time (s)")
 
-        settings=self.settings= Frame(root)
-        settings.pack(expand='yes')
+		settings=self.settings= Frame(root)
+		settings.pack(expand='yes')
 
-        # Make Slider Bars
-        off=self.offset= Scale(settings,from_=0, to=100,
-                               orient='horizontal',label = 'Offset')
-        amp=self.amp = Scale(settings,from_=0, to=100,
-                             orient='horizontal',label = 'Amp')
-        bpm=self.bpm = Scale(settings,from_=0, to=50,
-                             orient='horizontal',label = 'BPM')
-        off.grid(column=0,row=0)
-        amp.grid(column=1,row=0)
-        bpm.grid(column=2,row=0)
+		# Make Slider Bars
+		off=self.offset= Scale(settings,from_=0, to=100,
+							   orient='horizontal',label = 'Offset')
+		amp=self.amp = Scale(settings,from_=0, to=100,
+							 orient='horizontal',label = 'Amp')
+		bpm=self.bpm = Scale(settings,from_=0, to=50,
+							 orient='horizontal',label = 'BPM')
+		off.grid(column=0,row=0)
+		amp.grid(column=1,row=0)
+		bpm.grid(column=2,row=0)
 
-        variable= StringVar(settings)
-        variable.set("|sin|") # default value
+		variable= StringVar(settings)
+		variable.set("|sin|") # default value
 
-        mode = OptionMenu(settings, variable, "sin^2","sin^4","sin^6","|sin|",command=self.callback)
-        start = self.start = Button(settings, text ="Start", command = self.start)
-        update = self.update = Button(settings,text="Update",command=self.updatef)
-        mode.grid(column=3,row=0)
-        start.grid(column=4,row=0)
-        update.grid(column=5,row=0)
+		mode = OptionMenu(settings, variable, "sin^2","sin^4","sin^6","|sin|",command=self.callback)
+		start = self.start = Button(settings, text ="Start", command = self.start)
+		update = self.update = Button(settings,text="Update",command=self.updatef)
+		mode.grid(column=3,row=0)
+		start.grid(column=4,row=0)
+		update.grid(column=5,row=0)
 
-    def callback(self,modeselect):
-        self.FUNCinternal = modeselect
-        print(modeselect)
-    def updatef(self):
-        self.OFF = self.offset.get()*0.77+21
-        self.AMP = self.amp.get()*0.77
-        self.BPM = self.bpm.get()
-        self.FUNC = self.FUNCinternal
-        self.UpdateCheck = 1
+	def callback(self,modeselect):
+		self.FUNCinternal = modeselect
+		print(modeselect)
+	def updatef(self):
+		self.OFF = self.offset.get()*0.77+21
+		self.AMP = self.amp.get()*0.77
+		self.BPM = self.bpm.get()
+		self.FUNC = self.FUNCinternal
+		self.UpdateCheck = 1
 
-    def updatePlot(self,sTime,sData,eTime,eData):
-        t = self.t
-        f = self.f
-        ax = self.ax
-        canvas = self.canvas
+	def updatePlot(self,sTime,sData,eTime,eData):
+		t = self.t
+		f = self.f
+		ax = self.ax
+		canvas = self.canvas
 
-        if self.line == None:
-            self.sline, = ax.plot(sTime,sData,'b')
-            self.eline, = ax.plot(eTime,eData,'r')
+		if self.line == None:
+			self.sline, = ax.plot(sTime,sData,'b')
+			self.eline, = ax.plot(eTime,eData,'r')
 
-            ax.text(0.83, 1.02,'25 bpm',
-                    color = 'b', fontsize = 20,
-                    # bbox={'facecolor': 'blue', 'alpha': 1, 'pad': 3},
-                    transform = ax.transAxes)
+			ax.text(0.83, 1.02,'25 bpm',
+					color = 'b', fontsize = 20,
+					# bbox={'facecolor': 'blue', 'alpha': 1, 'pad': 3},
+					transform = ax.transAxes)
 
-            #ax.relim()
-            #ax.autoscale_view()
+			#ax.relim()
+			#ax.autoscale_view()
 
-            #canvas.draw()
-            #canvas.draw()
-            #canvas.flush_events()
-            ax.set_ylim([0, 100])
-            ax.set_xlim([min(sTime[0],eTime[0]), max(sTime[len(sTime)-1],eTime[len(eTime)-1])])
-
-
-            # set animation, save backgorund
-            ax.get_xaxis().set_animated(True)
-            self.sline.set_animated(True)
-            self.eline.set_animated(True)
-            canvas.draw()
-            self.background=canvas.copy_from_bbox(ax.get_figure().bbox)
-
-            # now redraw and blit
-            ax.draw_artist(ax.get_xaxis())
-            ax.draw_artist(self.sline)
-            ax.draw_artist(self.eline)
-            canvas.blit(ax.clipbox)
-
-        else:
-            self.sline.set_xdata(sTime)
-            self.sline.set_ydata(sData)
-            self.eline.set_xdata(eTime)
-            self.eline.set_ydata(eData)
-
-            ax.set_xlim([min(sTime[0],eTime[0]), max(sTime[len(sTime)-1],eTime[len(eTime)-1])])
+			#canvas.draw()
+			#canvas.draw()
+			#canvas.flush_events()
+			ax.set_ylim([0, 100])
+			ax.set_xlim([min(sTime[0],eTime[0]), max(sTime[len(sTime)-1],eTime[len(eTime)-1])])
 
 
-            # restore the background, draw animation,blit
-            canvas.restore_region(self.background)
-            ax.draw_artist(ax.get_xaxis())
-            ax.draw_artist(self.sline)
-            ax.draw_artist(self.eline)
-            canvas.blit(ax.clipbox)
-            canvas.flush_events()
+			# set animation, save backgorund
+			ax.get_xaxis().set_animated(True)
+			self.sline.set_animated(True)
+			self.eline.set_animated(True)
+			canvas.draw()
+			self.background=canvas.copy_from_bbox(ax.get_figure().bbox)
 
-    def start(self):
-        if self.running:
-            self.running = 0
-            self.start.configure(text='Start')
-            print("Stoping")
-        else:
-            self.running = 1
-            self.start.configure(text='Stop')
-            print("Starting")
-        self.updatef()
+			# now redraw and blit
+			ax.draw_artist(ax.get_xaxis())
+			ax.draw_artist(self.sline)
+			ax.draw_artist(self.eline)
+			canvas.blit(ax.clipbox)
 
-    def _close(self):
-        self.running = 0
-        self.UpdateCheck = 1
-        self.root.destroy()
+		else:
+			self.sline.set_xdata(sTime)
+			self.sline.set_ydata(sData)
+			self.eline.set_xdata(eTime)
+			self.eline.set_ydata(eData)
+
+			ax.set_xlim([min(sTime[0],eTime[0]), max(sTime[len(sTime)-1],eTime[len(eTime)-1])])
+
+
+			# restore the background, draw animation,blit
+			canvas.restore_region(self.background)
+			ax.draw_artist(ax.get_xaxis())
+			ax.draw_artist(self.sline)
+			ax.draw_artist(self.eline)
+			canvas.blit(ax.clipbox)
+			canvas.flush_events()
+
+	def start(self):
+		if self.running:
+			self.running = 0
+			self.start.configure(text='Start')
+			print("Stoping")
+		else:
+			self.running = 1
+			self.start.configure(text='Stop')
+			print("Starting")
+		self.updatef()
+
+	def _close(self):
+		self.running = 0
+		self.UpdateCheck = 1
+		self.root.destroy()
 
 
 class servofunctions:
@@ -227,15 +227,15 @@ class servofunctions:
 		self.time = self.time[-100:]
 
 class TeraRanger(Thread):
-    port = None
-    evo = None
-    data = []
-    time = []
-    start = 0
-    freqs = []
-    bpm = None
-    min = 100
-    max = 0
+	port = None
+	evo = None
+	data = []
+	time = []
+	start = 0
+	freqs = []
+	bpm = None
+	min = 100
+	max = 0
 
 	def __init__(self):
 
@@ -244,148 +244,148 @@ class TeraRanger(Thread):
 		self.port = self.findEvo()
 
 		if self.port == 'NULL':
-            print("Could not find Evo")
+			print("Could not find Evo")
 		else:
-            print("Found Evo")
-            self.evo = self.openEvo()
+			print("Found Evo")
+			self.evo = self.openEvo()
 
 	def run(self):
 		self.reset()
 		self.streamData()
 
 
-    def findEvo(self):
-        # Find Live Ports, return port name if found, NULL if not
-        print('Scanning all live ports on this PC')
-        ports = list(serial.tools.list_ports.comports())
-        for p in ports:
-            # print p # This causes each port's information to be printed out.
-            if "5740" in p[2]:
-                print('Evo found on port ' + p[0])
-                return p[0]
-        return 'NULL'
+	def findEvo(self):
+		# Find Live Ports, return port name if found, NULL if not
+		print('Scanning all live ports on this PC')
+		ports = list(serial.tools.list_ports.comports())
+		for p in ports:
+			# print p # This causes each port's information to be printed out.
+			if "5740" in p[2]:
+				print('Evo found on port ' + p[0])
+				return p[0]
+		return 'NULL'
 
-    def reset(self):
-        self.min = 100
-        self.max = 0
-        self.start = time()
-        self.data = []
-        self.time = []
+	def reset(self):
+		self.min = 100
+		self.max = 0
+		self.start = time()
+		self.data = []
+		self.time = []
 
-    def openEvo(self):
-        portname = self.port
-        print('Attempting to open port...')
-        # Open the Evo and catch any exceptions thrown by the OS
-        print(portname)
-        self.evo = serial.Serial(portname, baudrate=115200, timeout=2)
-        # Send the command "Binary mode"
-        set_bin = (0x00, 0x11, 0x02, 0x4C)
-        # Flush in the buffer
-        self.evo.flushInput()
-        # Write the binary command to the Evo
-        self.evo.write(set_bin)
-        # Flush out the buffer
-        self.evo.flushOutput()
-        print('Serial port opened')
-        return self.evo
+	def openEvo(self):
+		portname = self.port
+		print('Attempting to open port...')
+		# Open the Evo and catch any exceptions thrown by the OS
+		print(portname)
+		self.evo = serial.Serial(portname, baudrate=115200, timeout=2)
+		# Send the command "Binary mode"
+		set_bin = (0x00, 0x11, 0x02, 0x4C)
+		# Flush in the buffer
+		self.evo.flushInput()
+		# Write the binary command to the Evo
+		self.evo.write(set_bin)
+		# Flush out the buffer
+		self.evo.flushOutput()
+		print('Serial port opened')
+		return self.evo
 
-    def get_evo_range(self):
-        evo_serial = self.evo
-        crc8_fn = crcmod.predefined.mkPredefinedCrcFun('crc-8')
-        # Read one byte
-        evo_serial.flushInput()
-        data = evo_serial.read(1)
-        if data == b'T':
-            # After T read 3 bytes
-            frame = data + evo_serial.read(3)
-            if frame[3] == crc8_fn(frame[0:3]):
-                # Convert binary frame to decimal in shifting by 8 the frame
-                rng = frame[1] << 8
-                rng = rng | (frame[2] & 0xFF)
-            else:
-                print("CRC mismatch. Check connection or make sure only one progam access the sensor port.")
-                rng = 0
-        # Check special cases (limit values)
-        else:
-            print("Waiting for frame header")
-            rng = 0
+	def get_evo_range(self):
+		evo_serial = self.evo
+		crc8_fn = crcmod.predefined.mkPredefinedCrcFun('crc-8')
+		# Read one byte
+		evo_serial.flushInput()
+		data = evo_serial.read(1)
+		if data == b'T':
+			# After T read 3 bytes
+			frame = data + evo_serial.read(3)
+			if frame[3] == crc8_fn(frame[0:3]):
+				# Convert binary frame to decimal in shifting by 8 the frame
+				rng = frame[1] << 8
+				rng = rng | (frame[2] & 0xFF)
+			else:
+				print("CRC mismatch. Check connection or make sure only one progam access the sensor port.")
+				rng = 0
+		# Check special cases (limit values)
+		else:
+			print("Waiting for frame header")
+			rng = 0
 
-        # Checking error codes
-        #if rng == 65535: # Sensor measuring above its maximum limit
-        #    dec_out = float('inf')
-        #elif rng == 1: # Sensor not able to measure
-        #    dec_out = float('nan')
-        #elif rng == 0: # Sensor detecting object below minimum range
-        #    dec_out = -float('inf')
-        #else:
-            # Convert frame in meters
-        #    dec_out = rng / 1000.0
-
-
-        if rng == 65535 or rng == 1 or rng == 0:
-            dec_out = 0
-        else:
-            dec_out = rng / 1000.0
-
-        return dec_out
+		# Checking error codes
+		#if rng == 65535: # Sensor measuring above its maximum limit
+		#	dec_out = float('inf')
+		#elif rng == 1: # Sensor not able to measure
+		#	dec_out = float('nan')
+		#elif rng == 0: # Sensor detecting object below minimum range
+		#	dec_out = -float('inf')
+		#else:
+			# Convert frame in meters
+		#	dec_out = rng / 1000.0
 
 
+		if rng == 65535 or rng == 1 or rng == 0:
+			dec_out = 0
+		else:
+			dec_out = rng / 1000.0
 
-    def update(self):
-        # append to data array
-        self.data.append(self.get_evo_range())
-        self.data = self.data[-100:]
-
-        # append to time array
-        self.time.append(time()-self.start)
-        self.time = self.time[-100:]
-
-    def getData(self):
-        global edata
-        mi = min(self.data)
-        ma = max(self.data)
+		return dec_out
 
 
-        if mi < self.min and mi != 0:
-            self.min = mi
-        if ma > self.max:
-            self.max = ma
+
+	def update(self):
+		# append to data array
+		self.data.append(self.get_evo_range())
+		self.data = self.data[-100:]
+
+		# append to time array
+		self.time.append(time()-self.start)
+		self.time = self.time[-100:]
+
+	def getData(self):
+		global edata
+		mi = min(self.data)
+		ma = max(self.data)
 
 
-        temp = [0 for i in range(len(self.data))]
+		if mi < self.min and mi != 0:
+			self.min = mi
+		if ma > self.max:
+			self.max = ma
 
-        if self.max > 0:
-            if self.max != self.min:
-                for i in range(len(temp)):
-                    #temp[i] = (temp[i]-mi)*(100/ma)
-                    temp[i] = 100-(self.edata[i]-self.min)/(self.max-self.min)*100
-            else:
-                temp = self.edata
 
-        return temp
+		temp = [0 for i in range(len(self.data))]
 
-    def streamData(self):
-        global eData
-        global eTime
+		if self.max > 0:
+			if self.max != self.min:
+				for i in range(len(temp)):
+					#temp[i] = (temp[i]-mi)*(100/ma)
+					temp[i] = 100-(self.edata[i]-self.min)/(self.max-self.min)*100
+			else:
+				temp = self.edata
 
-        print('Starting Evo Data Stream')
+		return temp
 
-        while True:
-            try:
-                # append to data array
-                eData.append(self.get_evo_range())
-                eData = eData[-100:]
+	def streamData(self):
+		global eData
+		global eTime
 
-                # append to time array
-                eTime.append(time()-self.start)
-                eTime = eTime[-100:]
+		print('Starting Evo Data Stream')
 
-            except serial.serialutil.SerialException:
-                print("Device disconnected (or multiple access on port). Exiting...")
-                break
+		while True:
+			try:
+				# append to data array
+				eData.append(self.get_evo_range())
+				eData = eData[-100:]
 
-    def _close(self):
-        self.evo.close()
+				# append to time array
+				eTime.append(time()-self.start)
+				eTime = eTime[-100:]
+
+			except serial.serialutil.SerialException:
+				print("Device disconnected (or multiple access on port). Exiting...")
+				break
+
+	def _close(self):
+		self.evo.close()
 
 def switch(arg):
 	switcher = { "|sin|":servo.absolute,"sin^2":servo.squared,
